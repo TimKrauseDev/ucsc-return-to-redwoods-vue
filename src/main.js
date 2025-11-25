f344b31e7f124df36915e716618a5ce30e29a8b6
Tim Krause <timkrausedev@gmail.com>
2025-11-25 15:10:42 -0500

    add in primevue theme instead of unstyled

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/styles/site.scss';
 import 'primeicons/primeicons.css';
+import Lara from '@primeuix/themes/lara';
 
 import { createApp } from 'vue'
 import PrimeVue from 'primevue/config';
@@ -9,6 +10,10 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router);
-app.use(PrimeVue, { unstyled: true });
+app.use(PrimeVue, {
+  theme: {
+    preset: Lara,
+  }
+});
 
 app.mount('#app')
